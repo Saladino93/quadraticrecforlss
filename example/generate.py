a307cf5639172d2fefb83364313c1698af4eef25
Saladino93 <saladino_93@hotmail.it>
2020-02-08 13:30:27 +0000

    cleaning and polishing a bit

--- example/generate.py
+++ example/generate.py
@@ -1,3 +1,14 @@
+"""Compute various spectra and forecasting expressions for specific inputs.
+
+A directory must be specified on the command line. This directory must include:
+    - a file 'values.txt' with parameter values for bias and survey
+    - a file 'nonlinear_power.txt' with the nonlinear matter power spectrum at the z of interest
+    - a file 'linear_power.txt' with the linear matter power spectrum at the z of interest
+    - a file 'M.txt' with the M function at the z of interest
+
+The script dumps a pickle file of a dict with many quantities into 'data_dir/spectra.pickle'.
+"""
+
 import sys
 
 from quadforlss import estimator as es
@@ -15,14 +26,17 @@ import itertools
 
 import yaml
 
+#################################
+# Read input values and spectra
+#################################
 
+# Read forecast directory from command line
 
 if len(sys.argv) == 1:
     print('Choose your configuration file!')
     sys.exit()
 
-## Read configuration file
-
+# Read configuration file with yaml
 values_file = str(sys.argv[1])
 
 with open(values_file, 'r') as stream:
@@ -30,6 +44,7 @@ with open(values_file, 'r') as stream:
 
 values = data
 
+# Get directories
 direc = values['name']
 base_dir = values['file_config']['base_dir']
 data_dir = values['file_config']['data_dir']
@@ -40,32 +55,38 @@ direc = base_dir+direc+'/'
 
 #################
 
+# Get file names for nonlinear power spectrum, linear spectrum, and M(k)
 nonlinpowerfile = direc+data_dir+values['file_config']['nonlinear_power_name']
 linpowerfile = direc+data_dir+values['file_config']['linear_power_name']
 Mtransferfile = direc+data_dir+values['file_config']['M_name']
 
+# Read in P_nonlin and P_lin
 K, Pnlin = np.transpose(np.loadtxt(nonlinpowerfile))[0:2,:]
 Klin, Plin = np.transpose(np.loadtxt(linpowerfile))[0:2,:]
 Plin = np.interp(K, Klin, Plin)
 
+# Read in M
 kM, Mval = np.transpose(np.loadtxt(Mtransferfile))[0:2,:]
 Mscipy = scipy.interpolate.interp1d(kM, Mval)
 
 M = implemented_function('M', Mscipy)
 
+# Define min and max k values, making sure M is defined over the full k range
 minK = np.maximum(kM.min(), K.min())
 maxK = np.minimum(kM.max(), K.max())
 
+# Select k and power spectrum values within range defined above
 select = (K>minK)&(K<maxK)
 K = K[select]
 Plin = Plin[select]
 Pnlin = Pnlin[select]
 
 
-#################
+#################################
+# Set some parameter values
+#################################
 
-
-#print('Values are, ', values)
+# Fetch some values from the input dict
 
 
 minkh = values['analysis_config']['mink_analysis'] 
@@ -100,10 +121,8 @@ betaf = 2.*deltac*(b10-1.)
 
 variables_list = values['forecast_config']['variables_list']
 
-###Convert from sympy
 
-
-
+# If nonlinear bias values aren't specified, use theory predictions
 if bs2 == '':
    print('Using theory value for bs2!')
    bs2 = -2./7.*(b10-1)
@@ -140,7 +159,8 @@ fnl = float(fnl)
 
 ''' MODEL INPUT TOTAL POWER SPECTRUM 
 
-The total power spectrum will be given by the square b10+betaf*fnl/M times the NON linear power spectrum plus the shot noise contribution of the tracer
+The total power spectrum will be given by the square b10+betaf*fnl/M 
+times the NON linear power spectrum plus the shot noise contribution of the tracer
 
 '''
 
@@ -166,13 +186,23 @@ est = es.Estimator(K, Ptot, Plin)
 
 #this object is using sympy to define the different modecoupling kernels
 
-est.addF('g', 5./7.)
+# F_g = 17/21
+est.addF('g', 17./21.)
+# F_s = (1/2) * (q_2/q_1 + q_1/q_2) * \vec{q}_1\cdot\vec{q}_2 / (q_1 q_2)
 est.addF('s', 0.5*(est.q2/est.q1+est.q1/est.q2)*est.mu)
-est.addF('t', (2./7.)*est.mu**2.)
+# F_t = (2/7) * [ (\vec{q}_1\cdot\vec{q}_2)^2 / (q_1^2 q_2^2) - 1/3 ]
+est.addF('t', (2./7.)*est.mu**2.-1./3.)
+# F_{11} = (1/2) * (1/M(q_1) + 1/M(q_2))
 est.addF('c11', 0.5*(1./M(est.q1)+1./M(est.q2)))
-est.addF('c01', 0.5*est.mu*est.q1*est.q2*(1./(est.q1**2.*M(est.q2))+1./(est.q2**2.*M(est.q1))))
+# F_{01} = (1/2) * \vec{q}_1\cdot\vec{q}_2 / (q_1 q_2)
+#            * ( 1/(q_1^2 M(q_2)) + 1/(q_2^2 M(q_1)) )
+est.addF('c01', 0.5 * est.mu*est.q1*est.q2 \
+                * (1./(est.q1**2.*M(est.q2))+1./(est.q2**2.*M(est.q1))) )
+# F_{02} = 1 / (M(q_1) + M(q_2))
 est.addF('c02', (1./(M(est.q1)*M(est.q2))))
-est.addF('phiphi', M(sp.sqrt(est.q1**2.+est.q2**2.+2*est.q1*est.q2*est.mu))*(1./M(est.q1))*(1./M(est.q2)))
+# F_{\phi\phi} = M(|\vec{q}_1+\vec{q}_2|) / (M(q_1) M(q_2))
+est.addF('phiphi', M(sp.sqrt(est.q1**2.+est.q2**2.+2*est.q1*est.q2*est.mu)) \
+                    * (1./M(est.q1)) * (1./M(est.q2)) )
 
 
 #Which modes are reconstructed
@@ -253,87 +283,4 @@ sh_tris_3_a = (shotfactor_onePpower*Ngg)**2.*shot
 sh_tris_3_b = (shotfactor_zeroPpower*Ngg)*(shotfactor_twoPpower*Ngg)*shot
 
 sh_tris = sh_tris_1+4*sh_tris_2+4*sh_tris_3_a+2*sh_tris_3_b
-
-#######DICTIONARTY OF STUFF###########
-
-prefac = 1.
-
-M = Mscipy(est.Krange)
-
-values = np.array(list(est.keys))
-
-listKeys = list(itertools.combinations_with_replacement(list(values), 2))
-
-dic = {}
-
-dic['K'] = est.Krange
-
-for a, b in listKeys:
-    dic['N'+a+b] = prefac**2.*est.getN(a, b)
-    dic['N'+b+a] = dic['N'+a+b]
-
-B = betaf
-C = 4*deltac*((deltac/a1**2.)*(b20-2.*(a1+a2)*(bg-1.))-2.*(bg-1.))
-A = (2./a1)*(deltac*(b20-2*(a1+a2)*(bg-1.))-a1**2.*(bg-1.))+2.*deltac*(bg-1.)
-
-dic['minkh'] = minkh
-dic['maxkh'] = maxkh
-dic['minkhrec'] = minkhrec
-dic['maxkhrec'] = maxkhrec
-dic['z'] = z
-dic['ngal'] = nbar
-#from the loop parts
-dic['kphiphi'] = fnl*bg
-dic['kb01'] = fnl*B
-dic['kb11'] = fnl*A
-dic['kb02'] = fnl**2.*C
-
-dic['dfnlkphiphi'] = bg
-
-dic['bg'] = bg
-dic['ks'] = cs
-dic['kt'] = ct
-dic['kg'] = cg
-dic['b20'] = b20
-dic['bs2'] = bs2
-
-dic['fnl'] = fnl
-dic['bfnllargescales'] = betaf*fnl/M
-dic['derbfnllargescales'] = betaf/M
-dic['betaf'] = betaf
-dic['M'] = M
-
-Cgg = np.interp(est.Krange, K, Ptot)
-PL = np.interp(est.Krange, K, Plin)
-dfnlCgg = 2*(bg+(betaf*fnl*D(z))/M)*(betaf*D(z)/M)*PL
-
-terms = []
-for a in values:
-    terms += [dic['k'+a]*dic['Ngg']/dic['Ng'+a]]
-partial = prefac*bg*sum(terms)
-
-Cnn = (partial)**2.*PL+dic['Ngg']
-derpartialfnl = prefac*bg*((bg*dic['Ngg']/dic['Ngphiphi'])+A*(dic['Ngg']/dic['Ngb11'])+B*(dic['Ngg']/dic['Ngb01'])+2*fnl*C*(dic['Ngg']/dic['Ngb02']))
-dfnlCnn = 2*partial*derpartialfnl*PL
-
-Cgn = (bg+(betaf*fnl*D(z))/M)*partial*PL
-dfnlCgn = (bg+(betaf*fnl*D(z))/M)*derpartialfnl*PL+(betaf*D(z)/M)*partial*PL
-
-dic['Cgg'] = Cgg
-dic['Cnn'] = Cnn
-dic['Cgn'] = Cgn
-dic['dfnlCgg'] = dfnlCgg
-dic['dfnlCgn'] = dfnlCgn
-dic['dfnlCnn'] = dfnlCnn
-dic['PL'] = PL
-dic['shotnoise'] = shot
-dic['shot_noise_bispectrum'] = shot_noise_bispectrum
-dic['shot_noise_trispectrum'] = shot_noise_trispectrum
-dic['values'] = values
-
-
-with open(direc+'/data_dir/spectra.pickle', 'wb') as handle:
-    pickle.dump(dic, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-print('Done')
 '''
